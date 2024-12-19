ea3d0ffdcc780f76062dfc59d3c9a95e30d6a989
TechForEverybody <Pankajsinghat95@gmail.com>
2024-12-19 22:15:37 +0530

    sync

--- node.config.ts
+++ node.config.ts
@@ -2,7 +2,7 @@ import * as dotenv from 'dotenv'
 dotenv.config({ path: __dirname + '/.env' })
 
 const serverConfiguration = {
-    serverPort: parseInt(process.env.PORT || '9000') || 9000,
+    serverPort: parseInt(process.env.PORT || '7000') || 7000,
     serverHost: process.env.HOST || '127.0.0.1',
     secretKey: process.env.SECRET_KEY || 'Shivkumar Chauhan',
     databaseUrl: process.env.DATABASE_URL || '',
